e1c67a31ccb8ec1b19c81fc16a279fe769812b0f
Arturo Cordero <vakmancordero@gmail.com>
2018-08-22 11:40:01 -0500

    Adding SB project

---
+++ src/main/java/com/kaizensoftware/visitstory/VisitstoryApplication.java
@@ -0,0 +1,13 @@
+package com.kaizensoftware.visitstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitstoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisitstoryApplication.class, args);
+    }
+
+}
